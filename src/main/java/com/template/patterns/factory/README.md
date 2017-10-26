67c2da25c8d9fff8e4f5b367bb8252e8e9de5803
Marcos Rachid <marcosrachid@gmail.com>
2017-10-25 23:43:53 -0200

    Add Readme to each pattern

---
+++ src/main/java/com/template/patterns/factory/README.md
@@ -0,0 +1,3 @@
+# Singleton
+
+The singleton pattern is one of the simplest design patterns: it involves only one class which is responsible to instantiate itself, to make sure it creates not more than one instance; in the same time it provides a global point of access to that instance. In this case the same instance can be used from everywhere, being impossible to invoke directly the constructor each time.
--- src/main/java/com/template/patterns/singleton/Singleton.java
+++ src/main/java/com/template/patterns/singleton/Singleton.java
@@ -1,12 +1,6 @@
 package com.template.patterns.singleton;
 
 /**
- * The singleton pattern is one of the simplest design patterns: it involves
- * only one class which is responsible to instantiate itself, to make sure it
- * creates not more than one instance; in the same time it provides a global
- * point of access to that instance. In this case the same instance can be used
- * from everywhere, being impossible to invoke directly the constructor each
- * time.
  * 
  * @author marcosrachid
  *
