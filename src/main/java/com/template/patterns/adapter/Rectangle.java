06244491a169181073fe7bf1247c57040656e250
Marcos Rachid <marcosrachid@gmail.com>
2017-10-25 23:39:58 -0200

    Adapter pattern

---
+++ src/main/java/com/template/patterns/adapter/Rectangle.java
@@ -0,0 +1,15 @@
+package com.template.patterns.adapter;
+
+/**
+ * 
+ * @see <a href=
+ *      "https://sourcemaking.com/design_patterns/adapter/java/1">Adapter
+ *      reference</a>
+ *
+ */
+public class Rectangle {
+	public void draw(int x, int y, int width, int height) {
+		System.out.println("Rectangle with coordinate left-down point (" + x + ";" + y + "), width: " + width
+				+ ", height: " + height);
+	}
+}
